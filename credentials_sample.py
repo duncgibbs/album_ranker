1a15249ac6dbe82530fa9c306dcf94b3999af6ae
Duncan Gibbs <duncgibbs@gmail.com>
2016-12-22 10:21:02 -0600

    adds sample credentials file

---
+++ credentials_sample.py
@@ -0,0 +1,4 @@
+SPOTIFY_USER_ID='your-spotify-user-id'
+SPOTIPY_CLIENT_ID='your-spotify-client-id'
+SPOTIPY_CLIENT_SECRET='your-spotify-client-secret'
+SPOTIPY_REDIRECT_URI='your-spotify-redirect-uri'
